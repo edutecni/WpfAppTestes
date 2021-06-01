2ef942faa1802591e59897ac70065a15d14118fc
Eduardo Alves Canuto <edutecni@gmail.com>
2021-05-31 21:01:16 -0300

    Ok

---
+++ WpfAppTestes/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppTestes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //btnTeste.Background = new SolidColorBrush(Colors.Bisque);
+            byte red = 200; byte green = 70; byte blue = 90;
+            btnTeste.Background = new SolidColorBrush(Color.FromRgb(red, green, blue));
+        }
+    }
+}
